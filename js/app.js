967c4d3f4e2e46f2a56b6bc0ba1866564bef14c2
Valerii <valeraguess@gmail.com>
2020-08-26 16:09:37 +0300

    DOM Action

--- js/app.js
+++ js/app.js
@@ -700,3 +700,85 @@ const logger = function() {
 // alert( +"Infinity" ); // виведе строку
 
 // 0 || "" || 2 || undefined || true || falsе // 2
+
+//                        028 DOM
+
+// const box = document.getElementById('box');
+
+// console.log(box);
+
+// const btns = document.getElementsByTagName('button');
+
+
+// console.log(btns[1]);
+
+// const circles = document.getElementsByClassName('circle');
+
+// console.log(circles[1]);
+
+// const hearts = document.querySelectorAll('.heart');
+
+// console.log(hearts);
+
+// hearts.forEach(item => {
+//   console.log(item);
+// });
+
+// const oneHeart = document.querySelector('.heart');
+// console.log(oneHeart);
+
+//                       029 DOM Action
+
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
+
+
+  // box.style.backgroundColor = 'blue';
+  // box.style.width = '500px';
+
+  box.style.cssText = 'background-color: blue; width: 500px';
+    
+  btns[1].style.borderRadius = '100%';
+
+  circles[0].style.backgroundColor = 'red';
+
+  // for (let i = 0; i < hearts.length; i++) {
+  //   hearts[i].style.backgroundColor = 'blue';
+  // }
+
+  hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+  });
+
+  const div = document.createElement('div');
+  // const text = document.createTextNode('Im here');
+
+  div.classList.add('black');
+
+  // document.body.append(div);
+
+  wrapper.append(div); // new method
+  // wrapper.appendChild(div); // old
+
+  // wrapper.prepend(div);
+
+  // hearts[0].before(div);
+  // hearts[0].after(div);
+
+  // wrapper.insertBefore(div, hearts[0]); //old
+
+  // circles[0].remove();
+  // wrapper.removeChild(hearts[1]); //old
+
+  hearts[0].replaceWith(circles[0]);
+  // wrapper.replaceChild(circles[0], hearts[0]);
+
+  div.innerHTML = '<h1>Hello World</h1>';
+  
+  // div.textContent = 'hello';
+
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
