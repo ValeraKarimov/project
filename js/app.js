1f193d348d3186dc08a9b3185941b963cafec5b4
Valerii <valeraguess@gmail.com>
2020-09-09 12:27:58 +0300

    Classes ES6

--- js/app.js
+++ js/app.js
@@ -1143,3 +1143,37 @@ const obj = {
 }
 
 obj.sayNumber();
+
+//                  047 Classes ES6
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+
+  showMyProps() {
+    console.log(`Text: ${this.text}, Color: ${this.bgColor}`);
+  }
+}
+
+const div = new ColoredRectangleWithText(24, 10, 'Hello', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+
+// console.log(square.calcArea());
